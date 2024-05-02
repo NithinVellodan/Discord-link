ada9a2307686a8a9fa7b7167f05c5d0f57d71d65
nithinvellodan.talrop <nithinvellodan.talrop@gmail.com>
2024-05-02 11:57:17 +0530

    comit changes

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,9 @@
 export default function Main() {
   return (
     <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
-      <h1 className="text-3xl text-red-500-500 font-bold mb-2">Hello Guys</h1>
+      <h1 className="text-3xl text-red-500-500 font-bold mb-2">
+        Nithin is Super and fabulous
+      </h1>
     </div>
   );
 }

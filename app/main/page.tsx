6a4ea8f4dcc5b80ba9cf6105c486075865a244c5
nithinvellodan.talrop <nithinvellodan.talrop@gmail.com>
2024-05-02 11:38:04 +0530

    update commit

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,9 +1,7 @@
 export default function Main() {
-    return (
-        <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
-            <h1 className="text-3xl text-red-500-500 font-bold mb-2">
-                Nithin Vellodan 🤠
-            </h1>
-        </div>
-    );
+  return (
+    <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
+      <h1 className="text-3xl text-red-500-500 font-bold mb-2">Hello Guys</h1>
+    </div>
+  );
 }

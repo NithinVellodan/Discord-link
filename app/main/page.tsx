350d634c2a6f45517e43e558fa19457a064a64f4
nithinvellodan.talrop <nithinvellodan.talrop@gmail.com>
2024-05-02 13:03:54 +0530

    Lottie Updated

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+// Import your Lottie JSON file
+import yourLottieAnimation from "../../public/snake.json";
+
 export default function Main() {
+  //   const [state, setState] = useState(null);
+
   return (
-    <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
-      <h1 className="text-3xl text-red-500-500 font-bold mb-2">
-        Nithin is Super and fabulous
-      </h1>
+    <div className="flex flex-col h-full">
+      <div className="flex-1 relative">
+        {/* Lottie animation as background */}
+        {/* {state && ( */}
+        <Lottie
+          animationData={yourLottieAnimation}
+          style={{ position: "absolute", width: "100%", height: "100%" }}
+        />
+        {/* )} */}
+        <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 bg-amber-300"></div>
+        {/* Content */}
+        <div className="absolute top-0 left-0 w-full h-full py-3 px-3 flex items-center justify-center">
+          <h1 className="text-3xl text-red-950 font-bold mb-2">
+            Nithin is Super fabulous
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
+// import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+
+// export default function Home() {
+//     return (
+//         <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
+//             <h1 className="text-3xl text-indigo-500 font-bold mb-2">
+//                 Hello Discord Clone
+//             </h1>
+//             <Link href="/main">
+//                 <Button variant={"ghost"}>Click Me !</Button>
+//             </Link>
+//         </div>
+//     );
+// }
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div className="py-3 px-3 flex items-center flex-col h-full justify-center">
-            <h1 className="text-3xl text-indigo-500 font-bold mb-2">
-                Hello Discord Clone
-            </h1>
-            <Link href="/main">
-                <Button variant={"ghost"}>Click Me !</Button>
-            </Link>
-        </div>
-    );
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 to-purple-500 min-h-screen flex flex-col justify-center items-center">
+      <div className="text-center text-white">
+        {/* <Image
+          src="/discord_logo.png"
+          alt="Discord Logo"
+          width={100}
+          height={100}
+        /> */}
+        <h1 className="text-4xl font-bold mt-6 mb-4">
+          Welcome to Earn Daily Dollar Challenge
+        </h1>
+        <p className="text-lg mb-8">Experience seamless Earning</p>
+        <Link href="/main">
+          <Button
+            // variant="solid"
+            className="px-8 py-3 bg-white text-indigo-500 rounded-full hover:bg-indigo-400 hover:text-white transition duration-300 ease-in-out"
+          >
+            Get Started
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }

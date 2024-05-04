3fe15e619a5b52ebd7afce87ddf5641fe1956d11
nithinvellodan.talrop <nithinvellodan.talrop@gmail.com>
2024-05-04 17:01:01 +0530

    latst changes for discord

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -26,6 +26,7 @@ export default function Main() {
         <div className="absolute top-0 left-0 w-full h-full py-3 px-3 flex items-center justify-center">
           <h1 className="text-3xl text-red-950 font-bold mb-2">
             Hi ........Hello Moluuuuuuuuuuuuu hi hi nhiii Big Hiiiiiiiiiiiiiiii
+            hloo
           </h1>
         </div>
       </div>

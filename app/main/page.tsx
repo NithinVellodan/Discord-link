f3690b9fb4da649e470c84121d96e0c71442db58
nithinvellodan.talrop <nithinvellodan.talrop@gmail.com>
2024-05-06 11:07:04 +0530

    tested push

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -24,10 +24,7 @@ export default function Main() {
         <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 bg-amber-300"></div>
         {/* Content */}
         <div className="absolute top-0 left-0 w-full h-full py-3 px-3 flex items-center justify-center">
-          <h1 className="text-3xl text-red-950 font-bold mb-2">
-            Hi ........Hello Moluuuuuuuuuuuuu hi hi nhiii Big Hiiiiiiiiiiiiiiii
-            hloo
-          </h1>
+          <h1 className="text-3xl text-red-950 font-bold mb-2">test App</h1>
         </div>
       </div>
     </div>
